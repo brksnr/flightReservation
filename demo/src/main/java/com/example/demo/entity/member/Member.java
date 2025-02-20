8e58fa5bb3ff5e0c1a1f29025849eb37d72d98d2
brksnr <berksener04@gmail.com>
2025-02-20 16:12:18 +0300

    kullanıcı oluşturulma zamanı otomatik olarak ayarlanabilmesi için CreationTimestamp anotasyonu kullanıldı

--- demo/src/main/java/com/example/demo/entity/member/Member.java
+++ demo/src/main/java/com/example/demo/entity/member/Member.java
@@ -4,9 +4,8 @@ import com.example.demo.entity.Peyment;
 import com.example.demo.entity.Seat;
 import com.example.demo.entity.Travel;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -16,7 +15,8 @@ import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "member", schema = "flight")
 public class Member implements UserDetails {
@@ -36,6 +36,7 @@ public class Member implements UserDetails {
     private String password;
 
     @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
     private Timestamp createdAt;
 
     @OneToOne(mappedBy = "member", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
@@ -89,5 +90,69 @@ public class Member implements UserDetails {
     @Override
     public String getUsername() {
         return username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public MemberInfo getMemberInfo() {
+        return memberInfo;
+    }
+
+    public void setMemberInfo(MemberInfo memberInfo) {
+        this.memberInfo = memberInfo;
+    }
+
+    public List<Travel> getCreatedTravels() {
+        return createdTravels;
+    }
+
+    public void setCreatedTravels(List<Travel> createdTravels) {
+        this.createdTravels = createdTravels;
+    }
+
+    public List<Peyment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Peyment> payments) {
+        this.payments = payments;
+    }
+
+    public List<Seat> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<Seat> seats) {
+        this.seats = seats;
     }
 }
