9923092c80d4f72a16e96e130a4ec572d33dd415
brksnr <berksener04@gmail.com>
2025-02-20 19:21:34 +0300

    kullanıcının farklı bir member'ın bilgilerini değiştirmemesi için kontrol eklendi.

--- demo/src/main/java/com/example/demo/service/MemberInfoService.java
+++ demo/src/main/java/com/example/demo/service/MemberInfoService.java
@@ -24,16 +24,18 @@ public class MemberInfoService {
         this.memberInfoRepository = memberInfoRepository;
     }
 
-    public MemberInfo updateMemberInfo(Long id, MemberInfoRequest memberInfoRequest) {
+    public void updateMemberInfo(Long id, MemberInfoRequest memberInfoRequest) {
         Member member = memberRepository.findById(id)
-                .orElseThrow(() -> {
-                    throw new ApiException("Kullanıcı bulunamadı!", HttpStatus.NOT_FOUND);
-                });
+                .orElseThrow(() -> new ApiException("Kullanıcı bulunamadı!", HttpStatus.NOT_FOUND));
         MemberInfo memberInfo = member.getMemberInfo();
+
         if (memberInfo == null) {
             memberInfo = new MemberInfo();
-            member.setMemberInfo(memberInfo);
             memberInfo.setMember(member);
+            member.setMemberInfo(memberInfo);
+        }
+        if (!memberInfo.getId().equals(member.getId())) {
+            throw new ApiException("Farklı bir kullanıcının bilgilerini değiştiremezsiniz!", HttpStatus.FORBIDDEN);
         }
 
         memberInfo.setFullName(memberInfoRequest.fullName());
@@ -42,7 +44,7 @@ public class MemberInfoService {
         memberInfo.setHeight(memberInfoRequest.height());
         memberInfo.setWeight(memberInfoRequest.weight());
 
-        member.setMemberInfo(memberInfo);
-        return memberInfoRepository.save(memberInfo);
+        memberInfoRepository.save(memberInfo);
     }
+
 }
