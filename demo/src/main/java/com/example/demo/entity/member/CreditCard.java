d9658a504da059182049048edea8d11b22e9e5dc
brksnr <berksener04@gmail.com>
2025-02-21 13:14:52 +0300

    sonsuz döngüyü engellemek için @JsonBackReference anotasyonu kullanıldı.

--- demo/src/main/java/com/example/demo/entity/member/CreditCard.java
+++ demo/src/main/java/com/example/demo/entity/member/CreditCard.java
@@ -2,6 +2,7 @@ package com.example.demo.entity.member;
 
 import com.example.demo.entity.Peyment;
 import com.example.demo.entity.Travel;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -32,7 +33,7 @@ public class CreditCard {
     private String cardholderName;
 
     @NotNull
-    @Size(min = 16,max = 16, message = "Kart numarası 16 haneden oluşmalıdır!")
+    @Size(min = 19,max = 19, message = "Kart numarası 16 haneden oluşmalıdır!")
     @Column(name = "card_number")
     private String cardNumber;
 
@@ -47,6 +48,7 @@ public class CreditCard {
     private String cvc;
 
     @ManyToOne
+    @JsonBackReference
     @JoinColumn(name = "member_info_id")
     private MemberInfo memberInfo;
 
@@ -55,4 +57,76 @@ public class CreditCard {
 
     @OneToMany(mappedBy = "creditCard")
     private List<Peyment> payments;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCardholderName() {
+        return cardholderName;
+    }
+
+    public void setCardholderName(String cardholderName) {
+        this.cardholderName = cardholderName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getCvc() {
+        return cvc;
+    }
+
+    public void setCvc(String cvc) {
+        this.cvc = cvc;
+    }
+
+    public MemberInfo getMemberInfo() {
+        return memberInfo;
+    }
+
+    public void setMemberInfo(MemberInfo memberInfo) {
+        this.memberInfo = memberInfo;
+    }
+
+    public List<Travel> getTravels() {
+        return travels;
+    }
+
+    public void setTravels(List<Travel> travels) {
+        this.travels = travels;
+    }
+
+    public List<Peyment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Peyment> payments) {
+        this.payments = payments;
+    }
 }
